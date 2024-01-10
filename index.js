411e145622b444cdaf17c7f2e936c4b1caa6cd25
Aman Singh Parihar <amanparihar16@gmail.com>
2024-01-10 19:23:25 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function Show(){
+    var hamburger__menu = document.getElementById("hamburger__menu");
+    hamburger__menu.style.display = 'block';
+}
+
+function Hide(){
+    var hamburger__menu = document.getElementById("hamburger__menu");
+    hamburger__menu.style.display = 'none';
+}
